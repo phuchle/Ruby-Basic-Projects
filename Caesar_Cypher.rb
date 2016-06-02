5c9aee15cce2669ae69b014f1c731706663bcf4e
Phuc H. Le <le.phuc.h@gmail.com>
2016-06-01 19:25:59 -0700

    Write successful cypher and deciphermethods

--- Caesar_Cypher.rb
+++ Caesar_Cypher.rb
@@ -1,12 +1,52 @@
 def caesar_cypher(str, int)
-	alphabet = ("a".."z").to_a
+	alphabet = ("a".."z").to_a + ("A".."Z").to_a
+	translated = []
 	a = str.split('').each { |letter| 
-		if alphabet.include? letter {
-			letter.ord + int 
-			letter.chr
-		}
+		
+		if alphabet.include? letter
+			shifted = (letter.ord + int)
+			if shifted > 64 && shifted < 91 || shifted > 96 && shifted < 123
+				translated << shifted.chr
+			else
+				translated << (shifted - 26).chr
+			end
+		else
+			translated << letter
+		end
 	}
-	puts a.join
+	puts translated.join
 end
 
-caesar_cypher("What a string!", 5)
+def caesar_decypher(str, int)
+	alphabet = ("a".."z").to_a + ("A".."Z").to_a
+	translated = []
+	a = str.split('').each { |letter| 
+		if alphabet.include? letter
+			shifted = (letter.ord - int)
+			if shifted > 64 && shifted < 91 || shifted > 96 && shifted < 123
+				translated << shifted.chr
+			else
+				translated << (shifted - 26).chr
+			end
+		else
+			translated << letter
+		end
+	}
+	puts translated.join
+end
+
+puts "Enter text to be encrypted:"
+str = gets.chomp
+
+puts "Enter the shift factor:"
+int = gets.chomp.to_i
+
+puts caesar_cypher(str, int)
+
+puts "Enter text to be deciphered:"
+str = gets.chomp
+
+puts "Enter the shift factor:"
+int = gets.chomp.to_i
+
+puts caesar_decypher(str, int)
