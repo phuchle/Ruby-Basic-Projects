8747ea6ba90766be90e2e0ab25d06f33b7949f8c
Phuc H. Le <le.phuc.h@gmail.com>
2016-06-12 18:37:00 -0700

    Finish stock picker method.  Method sorts a given array to determine the highest price first, then determines a day before the highest price where the price is lowest.  It returns a string advising which day to buy and which day to sell for the highest profit margin.

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,23 +1,20 @@
 def stock_picker(array)
 	converted = []
-	picks = []
+	sorted = []
+	max = []
+	min = []
+	
 	array.each { |x| converted << x.to_i }
-	if converted.index(converted.max) < converted.index(converted.min) 
-		converted.delete(converted.max)
-		picks << converted.max
-	elsif converted.index(converted.max) == 0
-		converted.delete(converted.max)
-		picks << converted.max
-	elsif converted.index(converted.min) == -1
-		converted.delete(converted.min)
-		picks << converted.min
-	elsif converted.index(converted.max) > converted.index(converted.min)
-		picks << converted.min
-    end
-    puts picks
+	sorted = converted.sort
+	if sorted[-1] == converted[0]
+		max = sorted[-2]
+	else
+		max = sorted[-1]
+	end
+	
+	min = (converted[0..(converted.index(max))]).min
+
+	puts "It would be best to buy on day #{converted.index(min)} and sell on day #{converted.index(max)}."
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
-stock_picker([5,4,3,2,100])
-
-https://repl.it/CYRO/3
